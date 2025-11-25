823de27484e292895e91899ab84225724c423aef
kwtpub <10.0.kwt@mail.ru>
2025-11-25 17:19:14 +0300

    add bubble sort finish

--- 4 Лабораторная работа/src/main.c
+++ 4 Лабораторная работа/src/main.c
@@ -14,12 +14,41 @@ void generateArrays(int *randomArr, int *sortedArr, int *reverseArr, int n) {
 
 
 
+// Пузырьковая сортировка
 
 void bubbleSort(int *arr, int n, int showSteps) {
-    int i,j,temp, swapped;
+    int i,j,buf, swapped, counter1 = 0, counter2 = 0;
+    printf("Сортировка пузырьком\n");
     if(showSteps) {
-        printf("Исходный массив: ");
-        for(i = 0)
+        printf("\tИсходный массив: ");
+        for(i = 0; i < n; i++)
+            printf(" %d", arr[i]);
+        printf("\n");
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            counter1++;
+            if(arr[j] > arr[j+1]) {
+                counter2++;
+                buf = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = buf;
+            }
+            if(showSteps) {
+                printf("\tШаг %d: ", i + 1);
+                for(j = 0; j < n; j++)
+                    printf("%d ", arr[j]);
+                printf("\n");
+            }
+        }
+    }
+
+    if (showSteps) {
+        printf("\tОтсортированный массив: ");
+        for (i = 0; i < n; i++) printf("%d ", arr[i]);
+        printf("\n");
     }
 }
 
@@ -40,4 +69,6 @@ int main()
     int *sortedArr = (int *)malloc(n * sizeof(int));
     int *reverseArr = (int *)malloc(n * sizeof(int));
     generateArrays(randomArr, sortedArr, reverseArr, n);
+    bubbleSort(randomArr, n, 1);
+    return 0;
 }
