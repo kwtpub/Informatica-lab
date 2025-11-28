f32dfce4e6c80226aadf7eec178a0059054608d9
kwtpub <10.0.kwt@mail.ru>
2025-11-29 00:51:28 +0300

    add taskAll

---
+++ Контрольная работа/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+int main() {
+// вариант 2   ЗАДАЧА 1
+int x[4] = {11, 22, 33, 44};
+int *p;
+int y;
+printf("\n adress x=%p", x);
+printf("\n\n Адреса и значения элементов массива:\n\n");
+for (int i = 0; i < 4; i++) {
+    printf("\n &x[%d] = %p\t x[%d] = %d", i, &x[i], i, x[i]);
+}
+
+printf("\n\n3) adress &p = %p", &p);
+p = &x[0];   // p равно адресу элемента x[0]
+printf("\n значение *p = %d", *p);
+printf("\n\n3) значение указателя p=*p;  p = %p", p);
+printf("\n\n1) значение y = (*p)++;  p равно %p", p);
+y = (*p)++;
+printf("\n y = %d", y);
+printf("\n p = %p", p);
+printf("\n *p = %d", *p);
+
+// printf("\n\n2) значение y = *(++p)++;   p равно %p", p);
+// y = *((++p)++);
+printf("\n y = %d", y);
+
+printf("\n\n3) значение y = ++*p++;   p равно %p", p);
+y = ++*p++;
+printf("\n y = ???");
+
+printf("\n p = %p", p);
+printf("\n *p = %d", *p);
+
+printf("\n\nАдреса и значения элементов массива:");
+for (int i = 0; i < 4; i++) {
+    printf("\n &x[%d] = %p\t x[%d] = %d", i, &x[i], i, x[i]);
+}
+}
